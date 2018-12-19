6b14dcbaf7a673de17690481e1de5637092285a7
Michael Hazzard <miramardesign@gmail.com>
2018-12-19 19:10:37 -0300

    fix increment

--- public_html/harvest-time.gs.js
+++ public_html/harvest-time.gs.js
@@ -1,5 +1,5 @@
 
-function doHoursForLastMonth() {
+function invoiceHours() {
 
   //OPEN google spreadsheet template : tools-> script editor -paste in. 
   //add a trigger, on first of month. 
@@ -23,122 +23,118 @@ function doHoursForLastMonth() {
     emails: [
       'gianmarco@rockstarcoders.com'
     ]
-
+    
   };
 
-
   Number.prototype.pad = function (size) {
-    var s = String(this);
-    while (s.length < (size || 2)) { s = "0" + s; }
-    return s;
+      var s = String(this);
+      while (s.length < (size || 2)) { s = "0" + s; }
+      return s;
   }
 
   function getMonthName(i) {
-    var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-    return months[i];
+      return ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'][i];
   }
 
   // 0 = jan -> 01 = jan
   function zeroBasedMonthToPaddedMonth(zeroBasedMonth) {
-    var addedMonth = zeroBasedMonth + 1;
-    return addedMonth.pad(2);
+      var addedMonth = zeroBasedMonth + 1;
+      return addedMonth.pad(2);
   }
 
   //update from harvest using their api, get total hours for the last month,
   //https://help.getharvest.com/api-v2/authentication-api/authentication/authentication/
   function getHoursFromHarvest(dateLastMonth) {
 
-    //   curl -i \
-    //  -H 'Harvest-Account-ID: 268051'\
-    //  -H 'Authorization: Bearer '\
-    //  -H 'User-Agent: Harvest API Example' \
-    //  "https://api.harvestapp.com/api/v2/users/me.json"
-
-    //var url = 'https://api.harvestapp.com/api/v2/time_entries?from=2018-12-01&to=2018-12-07';
     var from = dateLastMonth.getYear() + '-' + zeroBasedMonthToPaddedMonth(dateLastMonth.getMonth()) + '-01';
 
-    var lastDayOfLastMonth = new Date(dateLastMonth.getFullYear(), dateLastMonth.getMonth() + 1, 0).getDate().pad(2);
+      var lastDayOfLastMonth = new Date(dateLastMonth.getFullYear(), dateLastMonth.getMonth() + 1, 0).getDate().pad(2);
 
-    var to = dateLastMonth.getYear() + '-' + zeroBasedMonthToPaddedMonth(dateLastMonth.getMonth()) + '-' + lastDayOfLastMonth;
+      var to = dateLastMonth.getYear() + '-' + zeroBasedMonthToPaddedMonth(dateLastMonth.getMonth()) + '-' + lastDayOfLastMonth;
 
-    var url = 'https://api.harvestapp.com/api/v2/time_entries?from=' + from + '&to=' + to;
+      var url = 'https://api.harvestapp.com/api/v2/time_entries?from=' + from + '&to=' + to;
 
-    var headers = {
-      "User-Agent": "Google Apps Script Harvest API Sample",
-      "Authorization": "Bearer " + config.harvest.accessToken,
-      "Harvest-Account-ID": config.harvest.accountID
-    };
+      var headers = {
+          "User-Agent": "Google Apps Script Harvest API Sample",
+          "Authorization": "Bearer " + config.harvest.accessToken,
+          "Harvest-Account-ID": config.harvest.accountID
+      };
 
-    var options = {
-      "method": "get",
-      "headers": headers
-    };
+      var options = {
+          "method": "get",
+          "headers": headers
+      };
 
-    var response = UrlFetchApp.fetch(url, options);
-    if (!response || !response.getContentText) {
-      throw 'could not get hours from harvest';
-    }
+      var response = UrlFetchApp.fetch(url, options);
+      if (!response || !response.getContentText) {
+          throw 'could not get hours from harvest';
+      }
 
-    var hoursHarvest = getHoursFromResponse(response.getContentText());
-    return hoursHarvest;
+      var hoursHarvest = getHoursFromResponse(response.getContentText());
+      return hoursHarvest;
   }
 
   function getHoursFromResponse(jsonTimeEntries) {
-    var jsonTimeEntriesObj = JSON.parse(jsonTimeEntries);
-    Logger.log('=====4=========jsonTimeEntriesObj[\'time_entries\']' + jsonTimeEntriesObj['time_entries']);
+      var jsonTimeEntriesObj = JSON.parse(jsonTimeEntries);
+      Logger.log('=====4=========jsonTimeEntriesObj[\'time_entries\']' + jsonTimeEntriesObj['time_entries']);
 
-    var hours = 0;
-    for (var i = 0; i < jsonTimeEntriesObj['time_entries'].length; i++) {
-      var thisTime = jsonTimeEntriesObj['time_entries'][i];
-      Logger.log('thisTime', thisTime['hours']);
-      hours += thisTime['hours'];
-    }
+      var hours = 0;
+      for (var i = 0; i < jsonTimeEntriesObj['time_entries'].length; i++) {
+          var thisTime = jsonTimeEntriesObj['time_entries'][i];
+          Logger.log('thisTime', thisTime['hours']);
+          hours += thisTime['hours'];
+      }
 
-    Logger.log('HOURS added  RAWWWW' + hours);
-    return Math.ceil(hours);
+      Logger.log('HOURS added  RAWWWW' + hours);
+      return Math.ceil(hours);
   }
 
   function init(monthsBack) {
 
-    //clone the doc at the beginning of the month
-    var dateLastMonth = new Date();
-    dateLastMonth.setMonth(dateLastMonth.getMonth() - monthsBack);
+      //increment invoice num before cloning dock.
+      var ssOrig = SpreadsheetApp.getActiveSpreadsheet();
 
-    var monthName = getMonthName(dateLastMonth.getMonth())
-    var fileName = config.user.name + ' for ' + monthName + ' ' + dateLastMonth.getYear() + ' ' + dateLastMonth.toLocaleTimeString();
+      var lastInvoice = ssOrig.getRange('B2').getValue();
+      ssOrig.getRange('B2').setValue(lastInvoice + 1);
+      SpreadsheetApp.flush(); //save the sheet
 
-    var file = DriveApp.getFileById(config.drive.template); //'Michael Hazzard Invoice -- Template'
-    //put your template invoice in a folder in drive/ click on it and get the id from the url
-    var folder = DriveApp.getFolderById(config.drive.folder); // 'invoices'
+      //clone the doc at the beginning of the month
+      var dateLastMonth = new Date();
+      dateLastMonth.setMonth(dateLastMonth.getMonth() - monthsBack);
 
-    var newFile = file.makeCopy(fileName, folder);
+      var monthName = getMonthName(dateLastMonth.getMonth())
+      var fileName = config.user.name + ' for ' + monthName + ' ' + dateLastMonth.getYear() + ' ' + dateLastMonth.toLocaleTimeString();
 
-    var ss = SpreadsheetApp.openById(newFile.getId());
-    SpreadsheetApp.setActiveSpreadsheet(ss);
+      var file = DriveApp.getFileById(config.drive.template); //'Michael Hazzard Invoice -- Template'
+      file
+      //put your template invoice in a folder in drive/ click on it and get the id from the url
+      var folder = DriveApp.getFolderById(config.drive.folder); // 'invoices'
 
-    //set hours put in cell G17, which is calced auto
-    var hoursWorked = getHoursFromHarvest(dateLastMonth);
-    ss.getRange('G17').setValue(hoursWorked);
+      var newFile = file.makeCopy(fileName, folder);
 
-    //just adding timestamp to invoice num rather than doing persistence. todo: make an increment?
-    ss.getRange('B2').setValue(dateLastMonth.getTime() / 1000);
+      var ssClone = SpreadsheetApp.openById(newFile.getId());
+      SpreadsheetApp.setActiveSpreadsheet(ssClone);
 
-    //set description in cell C17, may get from data? todo.
-    var descriptionWorked = config.project + monthName + ' ' + dateLastMonth.getYear();
-    ss.getRange('C17').setValue(descriptionWorked);
+      //set hours put in cell G17, which is calced auto
+      var hoursWorked = getHoursFromHarvest(dateLastMonth);
+      ssClone.getRange('G17').setValue(hoursWorked);
+     
+      //set description in cell C17, may get from data? todo.
+      var descriptionWorked = config.project + monthName + ' ' + dateLastMonth.getYear();
+      ssClone.getRange('C17').setValue(descriptionWorked);
 
-    //share to emails list if 1st of month.,  otherwise testing and not spamming.
-    if (dateLastMonth.getDate() === 1) {
-      newFile.addViewer(config.emails[0]);
-    }
+      //share to emails list if 1st of month.,  otherwise testing and not spamming.
+      if(dateLastMonth.getDate() === 1){
+        newFile.addViewer(config.emails[0]);
+      }
   }
 
   //test 6 months to compare.
   //for(var i = 1; i < 7; i++){
   //  init(i);
   //}
-
+  
   //last month 1 back.
   init(1);
-
+ 
 }
